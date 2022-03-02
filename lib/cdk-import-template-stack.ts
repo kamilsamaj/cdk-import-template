2432db7a0d080692bf0c9f6bf8b9580afd01f522
kamilsamaj <kamilsamaj@gmail.com>
2022-03-02 15:41:32 +0100

    import an external template.yml

--- lib/cdk-import-template-stack.ts
+++ lib/cdk-import-template-stack.ts
@@ -1,16 +1,25 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3, Stack, StackProps } from "aws-cdk-lib";
+import * as cfninc from "aws-cdk-lib/cloudformation-include";
+import { Construct } from "constructs";
 
 export class CdkImportTemplateStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const template = new cfninc.CfnInclude(this, "Template", {
+      templateFile: "template.yml",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkImportTemplateQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cfnBucket = template.getResource("S3Bucket") as s3.CfnBucket;
+    cfnBucket.bucketEncryption = {
+      serverSideEncryptionConfiguration: [
+        {
+          bucketKeyEnabled: false,
+          serverSideEncryptionByDefault: {
+            sseAlgorithm: "AES256",
+          },
+        },
+      ],
+    };
   }
 }
